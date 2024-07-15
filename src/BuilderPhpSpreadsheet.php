39037e0e776fded54a288e690afcf08332b61bc3
mins01 <mins01.lycos.co.kr@gmail.com>
2024-07-15 17:35:30 +0900

    first

---
+++ src/BuilderPhpSpreadsheet.php
@@ -0,0 +1,173 @@
+<?
+
+class BuilderPhpSpreadsheet{
+    public $spreadsheet = null;
+    public $default_width = 20;
+
+    static $defStyles = [
+        'alignmentCenterCenter' => [
+            'alignment' => [
+                'horizontal' => PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_CENTER,
+                'vertical' => PhpOffice\PhpSpreadsheet\Style\Alignment::VERTICAL_CENTER,
+            ],
+        ],
+        'fillForHeader' =>[
+            'fill'=>[
+                'fillType'=>\PhpOffice\PhpSpreadsheet\Style\Fill::FILL_SOLID,
+                'color' => array('rgb' => 'C6EFCE'),
+            ]
+        ],
+        'fillForFooter' =>[
+            'fill'=>[
+                'fillType'=>\PhpOffice\PhpSpreadsheet\Style\Fill::FILL_SOLID,
+                'color' => array('rgb' => 'eeeeee'),
+            ]
+        ],
+        'bordersAll'=>[
+            'borders' => [
+                'allBorders' => [
+                    'borderStyle' => \PhpOffice\PhpSpreadsheet\Style\Border::BORDER_THIN,
+                    'color' => ['argb' => '00000000'],
+                ],
+            ],
+        ],
+        'fontForLink'=>[
+            'font'  => [
+                    'color' => ['rgb' => '0000FF'],
+                    'underline' => 'single',
+            ],
+        ]
+    ];
+    public $defConf = [];
+
+
+    public function __construct() {
+        $this->spreadsheet = new PhpOffice\PhpSpreadsheet\Spreadsheet();
+        $this->init();
+    }
+
+    private function init(){
+        $this->spreadsheet->getProperties()->setCreator('BuilderPhpSpreadsheet')
+        ->setLastModifiedBy('BuilderPhpSpreadsheet')
+        ->setTitle('BuilderPhpSpreadsheet')
+        ->setSubject('BuilderPhpSpreadsheet')
+        ->setDescription('BuilderPhpSpreadsheet')
+        ->setKeywords('BuilderPhpSpreadsheet')
+        ->setCategory('BuilderPhpSpreadsheet')
+        ->setDescription(
+            "CREATED BY BuilderPhpSpreadsheet"
+        );
+        // $this->spreadsheet->removeSheetByIndex(1);
+
+        $this->defConf = [
+            'header'=>['style'=>static::$defStyles['fillForHeader']+static::$defStyles['bordersAll']+static::$defStyles['alignmentCenterCenter']],
+            'body'=>['style'=>static::$defStyles['bordersAll'],],
+            'footer'=>['style'=>static::$defStyles['fillForFooter']+static::$defStyles['bordersAll']],
+        ];
+    }
+    public function download($filename,$type='xlsx'){
+        if($type=='xlsx'){
+            // 다운로드 - xlsx
+            $writer = new PhpOffice\PhpSpreadsheet\Writer\Xlsx($this->spreadsheet);
+            header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+            header('Content-Disposition: attachment;filename="'.$filename.'.xlsx"');
+            header('Cache-Control: max-age=0');
+            $writer->save('php://output');
+        }else if($type=='html'){
+            // 테스트 출력부 - HTML
+            $writer = new PhpOffice\PhpSpreadsheet\Writer\Html($this->spreadsheet);    
+            $writer->save('php://output');
+        }
+    }
+    public function save($filename,$type='xlsx'){
+        if($type=='xlsx'){
+            // 다운로드 - xlsx
+            $writer = new PhpOffice\PhpSpreadsheet\Writer\Xlsx($this->spreadsheet);
+            $writer->save($filename.'.xlsx');
+        }else if($type=='html'){
+            // 테스트 출력부 - HTML
+            $writer = new PhpOffice\PhpSpreadsheet\Writer\Html($this->spreadsheet);    
+            $writer->save($filename.'.html');
+        } 
+    }
+
+    public function setSheetData($idx,$conf=null,$body=null,$header=null,$footer=null){
+        $sheet =  null;
+        if($idx <= $this->spreadsheet->getSheetCount()-1){
+            $sheet = $this->spreadsheet->getSheet($idx);
+        }else{
+            $sheet = $this->spreadsheet->createSheet($idx);
+        }
+        if(!$sheet){
+            throw new Exception( "Not exists sheet.({$idx})");
+        }
+        return $this->setSheetDataFromSheet($sheet,$conf,$body,$header,$footer);
+    }
+    public function setSheetDataFromSheet($sheet,$conf=null,$body=null,$header=null,$footer=null){
+        if(!isset($this->default_width)) $sheet->getDefaultColumnDimension()->setWidth($this->default_width); // 각 시트에 기본 너비 설정함
+        
+        
+        //-- 시트 설정
+        if(isset($conf['sheet']['title'])){ $sheet->setTitle($conf['sheet']['title']);}
+        //--- 시트 column 설정
+        $cf = $conf['sheet']??null;
+        if(isset($cf['columns'])){
+            foreach($cf['columns'] as $k => $column){
+                $cIdx = $k+1; //column idx
+                if(isset($column['width'])){
+                    if($column['width'] =='auto'){
+                        $sheet->getColumnDimensionByColumn($cIdx)->setAutoSize(true);
+                    }else{
+                        $sheet->getColumnDimensionByColumn($cIdx)->setWidth($column['width']);
+                    }
+                }            
+            }
+        }
+        $rIdx = 1; // row idx
+
+        if(isset($header[0][0])){
+            $partConf = ($conf['header']??[])+$this->defConf['header'];
+            $rIdx = $this->setSheetDataPart($sheet,$rIdx,$header,$partConf);
+        }
+        if(isset($body[0][0])){
+            $partConf = ($conf['body']??[])+$this->defConf['body'];
+            $rIdx = $this->setSheetDataPart($sheet,$rIdx,$body,$partConf);
+        }
+        if(isset($footer[0][0])){
+            $partConf = ($conf['footer']??[])+$this->defConf['footer'];
+            $rIdx = $this->setSheetDataPart($sheet,$rIdx,$footer,$partConf);
+        }
+        $sheet->setSelectedCell('A1');
+        return $sheet;
+    }
+
+    /**
+     * 시트에 데이터 파트를 적용
+     */
+    public function setSheetDataPart($sheet,$rIdx,$partValues,$partConf){
+        //-- 값 설정
+        $d = & $partValues;
+        $firstCellCoord = 'A'.$rIdx;
+        $sheet->fromArray($d,null,'A'.$rIdx);
+        $rIdx+= count($d);
+        $lastColIndex = max(array_map(function($arr){return count($arr);},$d));
+        $lastColAlpha = \PhpOffice\PhpSpreadsheet\Cell\Coordinate::stringFromColumnIndex($lastColIndex);
+        $lastCellCoord = $lastColAlpha.($rIdx-1);
+        //-- 파트의 스타일
+        if(isset($partConf['style'])) $sheet->getStyle("{$firstCellCoord}:{$lastCellCoord}")->applyFromArray($partConf['style']);
+        //-- 컬럼의 스타일
+        if($partConf && isset($partConf['columns'])){
+            foreach($partConf['columns'] as $k => $column ){
+                if(!isset($column)) continue;
+                $alpha = \PhpOffice\PhpSpreadsheet\Cell\Coordinate::stringFromColumnIndex($k+1);
+                $colCoord = $alpha.($rIdx - count($d)).':'.$alpha.($rIdx - 1);
+                if(isset($column['style'])){
+                    $sheet->getStyle($colCoord)->applyFromArray($column['style']);
+                }
+            }
+        }
+        unset($d);
+        return $rIdx;
+    }
+    
+}
---
+++ test/test.BuilderPhpSpreadsheet.php
@@ -0,0 +1,175 @@
+<?
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+require_once('../vendor/autoload.php');
+require_once('../src/BuilderPhpSpreadsheet.php');
+
+
+$bps = new BuilderPhpSpreadsheet();
+
+$conf = [
+    'sheet'=>[
+        'title'=>"설정없음",
+    ],
+];
+$header = [
+    ['해더1-'.time(),'해더2','해더3','해더4','해더5','해더6','해더7','해더8','해더9','해더10','해더11','해더12','해더13',],
+    ['해더:2-1','해더:2-2','해더:2-3','해더:2-4','해더:2-5','해더:2-6','해더:2-7','해더:2-8','해더:2-9','해더:2-10','해더:2-11','해더:2-12','해더:2-13',]
+];
+$footer = [
+    ['풋터1','풋터2','풋터3','풋터4','풋터5','풋터6','풋터7','풋터8','풋터9','풋터10','풋터11','풋터12','풋터13',],
+    ['풋터:2-1','풋터:2-2','풋터:2-3','풋터:2-4','풋터:2-5','풋터:2-6','풋터:2-7','풋터:2-8','풋터:2-9','풋터:2-10','풋터:2-11','풋터:2-12','풋터:2-13',]
+];
+$body = [
+    [1,2,3,4,5],
+    ['데이터1','22222222','2024-07-15','데이터4','오른쪽 정렬','데이터6','데이터7','데이터8','데이터9','데이터10','데이터11','데이터12','데이터13',],
+];
+$bps->setSheetData(0,$conf,$body,$header,$footer);
+
+
+
+
+$conf = [
+    'sheet'=>[
+        'title'=>"컬럼너비만 설정",
+        'columns'=>[
+            ['width'=>10],
+            ['width'=>20],
+            ['width'=>30],
+            ['width'=>40],
+            ['width'=>'auto'],
+            ['width'=>30],
+            ['width'=>20],
+            ['width'=>10],
+            ['width'=>5],
+            [],
+            [],
+        ],
+    ],
+];
+$header = [
+    ['해더1-'.time(),'해더2','해더3','해더4','해더5','해더6','해더7','해더8','해더9','해더10','해더11','해더12','해더13',],
+    ['해더:2-1','해더:2-2','해더:2-3','해더:2-4','해더:2-5','해더:2-6','해더:2-7','해더:2-8','해더:2-9','해더:2-10','해더:2-11','해더:2-12','해더:2-13',]
+];
+$footer = [
+    ['풋터1','풋터2','풋터3','풋터4','풋터5','풋터6','풋터7','풋터8','풋터9','풋터10','풋터11','풋터12','풋터13',],
+    ['풋터:2-1','풋터:2-2','풋터:2-3','풋터:2-4','풋터:2-5','풋터:2-6','풋터:2-7','풋터:2-8','풋터:2-9','풋터:2-10','풋터:2-11','풋터:2-12','풋터:2-13',]
+];
+$body = [
+    ['10','20','30','40','auto','30','20','10','5','너비설정없음','너비설정없음','설정없음','설정없음',],
+    ['데이터1','22222222','2024-07-15','설정이 없으면 기본너비 사용','오른쪽 정렬','데이터6','데이터7','데이터8','데이터9','데이터10','데이터11','데이터12','데이터13',],
+    ['데이터1','22222222','2024-07-15','데이터4','오른쪽 정렬','데이터6','데이터7','데이터8','데이터9','데이터10','데이터11','데이터12','데이터13',],
+
+    
+];
+// downloadXlsx($fn,$conf,$body,$header,$footer);
+$bps->setSheetData(1,$conf,$body,$header,$footer);
+
+
+
+$conf = [
+    'sheet'=>[
+        'title'=>"복잡하게 설정",
+        'columns'=>[
+            ['width'=>10],
+            ['width'=>20],
+            ['width'=>30],
+            ['width'=>40],
+            ['width'=>'auto'],
+            [],
+            [],
+            [],
+            [],
+            [],
+            [],
+        ],
+    ],
+    
+    'header'=>[
+        'columns'=>[
+            ['style'=>[ 'font' => [ 'color'=>[ 'argb'=>PhpOffice\PhpSpreadsheet\Style\Color::COLOR_RED, ], ], ],],
+        ]
+    ],
+    'footer'=>[
+        'columns'=>[
+            null,
+            null,
+            ['style'=>[ 'font' => [ 'color'=>[ 'argb'=>PhpOffice\PhpSpreadsheet\Style\Color::COLOR_BLUE, ], ], ],],
+        ],
+        'style'=>[ 
+            'font' => [ 
+                'bold'=>true, 
+                'color'=>[
+                    'argb'=>PhpOffice\PhpSpreadsheet\Style\Color::COLOR_GREEN,
+                ]
+            ], 
+        ],
+
+    ],
+    'body'=>[
+        'columns'=>[
+            ['style'=>['numberFormat'=>['formatCode'=>PhpOffice\PhpSpreadsheet\Style\NumberFormat::FORMAT_TEXT,]]],
+            ['style'=>['numberFormat'=>['formatCode'=>PhpOffice\PhpSpreadsheet\Style\NumberFormat::FORMAT_NUMBER,]]],
+            ['style'=>[
+                    'numberFormat'=>['formatCode'=>PhpOffice\PhpSpreadsheet\Style\NumberFormat::FORMAT_DATE_YYYYMMDD,],
+                    'alignment' => [
+                        'horizontal' => PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_CENTER,
+                        'vertical' => PhpOffice\PhpSpreadsheet\Style\Alignment::VERTICAL_CENTER,
+                    ],
+                ]
+            ],
+            null,
+            ['style'=>[
+                    'alignment' => [ 'horizontal' => PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_RIGHT, ],
+                ]
+            ],
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+        ]
+    ]
+];
+$header = [
+    ['해더1-'.time(),'해더2','해더3','해더4','해더5','해더6','해더7','해더8','해더9','해더10','해더11','해더12','해더13',],
+    ['해더:2-1','해더:2-2','해더:2-3','해더:2-4','해더:2-5','해더:2-6','해더:2-7','해더:2-8','해더:2-9','해더:2-10','해더:2-11','해더:2-12','해더:2-13',],
+    [null,null,null,'해더컬럼스타일'],
+];
+$footer = [
+    ['풋터1','풋터2','풋터3','풋터4','풋터5','풋터6','풋터7','풋터8','풋터9','풋터10','풋터11','풋터12','풋터13',],
+    ['풋터:2-1','풋터:2-2','풋터:2-3','풋터:2-4','풋터:2-5','풋터:2-6','풋터:2-7','풋터:2-8','풋터:2-9','풋터:2-10','풋터:2-11','풋터:2-12','풋터:2-13',],
+    [null,null,null,'풋터컬럼스타일,풋터전체스타일재설정'],
+];
+$body = [
+    ['데이터1','22222222','2024-07-15','데이터4','오른쪽 정렬','데이터6','데이터7','데이터8','데이터9','데이터10','데이터11','데이터12','데이터13',],
+    ['데이터1','22222222','2024-07-15','데이터4','오른쪽 정렬','데이터6','데이터7','데이터8','데이터9','데이터10','데이터11','데이터12','데이터13',],
+    ['데이터1','22222222','2024-07-15','데이터4','오른쪽 정렬','데이터6','데이터7','데이터8','데이터9','데이터10','데이터11','데이터12','데이터13',],
+    [null,null,null,'컬럼별 formatCode 설정'],
+
+    
+];
+$bps->setSheetData(2,$conf,$body,$header,$footer);
+
+
+
+//-- 셀 머지 예제. (BuilderPhpSpreadsheet 에서는 지원안하니깐.. 수동으로 처리한다.)
+$spreadsheet = $bps->spreadsheet;
+$sheet = $spreadsheet->getSheet(2);
+$sheet->mergeCells('A4:C6');
+$sheet->setCellValue('A4','셀 머지');
+$sheet->getStyle('A4')->applyFromArray(BuilderPhpSpreadsheet::$defStyles['alignmentCenterCenter']);
+
+
+
+//--- xlsx 이면 다운로드, html 이면 브라우저에서 그냥 보여진다.
+// $bps->download('testExcel','html');
+// $bps->download('testExcel','xlsx');
+$bps->save('testExcel','xlsx');
+$bps->save('testExcel','html');
+
